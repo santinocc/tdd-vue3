e28dc27c3ccf8334a460d39c274eb0773d32d2a2
Santino <santino.cervera@gmail.com>
2022-06-04 13:39:37 +0300

    4th Making Api Request & Test

--- src/pages/SignUpPage.vue
+++ src/pages/SignUpPage.vue
@@ -1,10 +1,11 @@
 <template>
+<form>
   <h1>Sign Up</h1>
   <label for="username">Username</label>
-  <input id="username" />
+  <input id="username" v-model="username"/>
 
   <label for="e-mail">E-mail</label>
-  <input id="e-mail" />
+  <input id="e-mail" v-model="email"/>
 
   <label for="password">Password</label>
   <input id="password" type="password" v-model="password"/>
@@ -12,17 +13,30 @@
   <label for="password-repeat">Password Repeat</label>
   <input id="password-repeat" type="password" v-model="passwordRepeat"/>
 
-  <button :disabled="isDisabled">Sign Up</button>
+  <button :disabled="isDisabled" @click.prevent="submit">Sign Up</button>
+</form>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "SignUpPage",
   data() {
     return {
+      username: '',
+      email: '',
       password: '',
-      passwordRepeat: ''
+      passwordRepeat: '',
     };
+  },
+  methods: {
+    submit(){
+      axios.post("/api/1.0/users",{
+        username: this.username,
+        email: this.email,
+        password: this.password
+      })
+    },
   },
   computed: {
     isDisabled(){
