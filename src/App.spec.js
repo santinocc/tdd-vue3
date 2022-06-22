64e2747ba5f19c30af53ddf8b4d34888e08ba0af
Santino <santino.cervera@gmail.com>
2022-06-23 00:21:23 +0300

    29th Account Activation Page

--- src/App.spec.js
+++ src/App.spec.js
@@ -14,33 +14,44 @@ const setup = async (path) => {
 
 describe("Routing", () => {
   it.each`
-    path         | pageTestId
-    ${"/"}       | ${"home-page"}
-    ${"/signup"} | ${"signup-page"}
-    ${"/login"}  | ${"login-page"}
-    ${"/user/1"} | ${"user-page"}
-    ${"/user/2"} | ${"user-page"}
+    path                | pageTestId
+    ${"/"}              | ${"home-page"}
+    ${"/signup"}        | ${"signup-page"}
+    ${"/login"}         | ${"login-page"}
+    ${"/user/1"}        | ${"user-page"}
+    ${"/user/2"}        | ${"user-page"}
+    ${"/activate/1234"} | ${"activation-page"}
+    ${"/activate/5678"} | ${"activation-page"}
   `("displays $pageTestId when path is $path", async ({ path, pageTestId }) => {
     await setup(path);
     const page = screen.queryByTestId(pageTestId);
     expect(page).toBeInTheDocument();
   });
   it.each`
-    path         | pageTestId
-    ${"/"}       | ${"signup-page"}
-    ${"/"}       | ${"login-page"}
-    ${"/"}       | ${"user-page"}
-    ${"/signup"} | ${"home-page"}
-    ${"/signup"} | ${"login-page"}
-    ${"/signup"} | ${"user-page"}
-    ${"/login"}  | ${"home-page"}
-    ${"/login"}  | ${"signup-page"}
-    ${"/login"}  | ${"user-page"}
-    ${"/user/1"} | ${"home-page"}
-    ${"/user/1"} | ${"signup-page"}
-    ${"/user/1"} | ${"login-page"}
+    path               | pageTestId
+    ${"/"}             | ${"signup-page"}
+    ${"/"}             | ${"login-page"}
+    ${"/"}             | ${"user-page"}
+    ${"/"}             | ${"activation-page"}
+    ${"/signup"}       | ${"home-page"}
+    ${"/signup"}       | ${"login-page"}
+    ${"/signup"}       | ${"user-page"}
+    ${"/signup"}       | ${"activation-page"}
+    ${"/login"}        | ${"home-page"}
+    ${"/login"}        | ${"signup-page"}
+    ${"/login"}        | ${"user-page"}
+    ${"/login"}        | ${"activation-page"}
+    ${"/user/1"}       | ${"home-page"}
+    ${"/user/1"}       | ${"signup-page"}
+    ${"/user/1"}       | ${"login-page"}
+    ${"/user/1"}       | ${"activation-page"}
+    ${"/activate/123"} | ${"signup-page"}
+    ${"/activate/123"} | ${"home-page"}
+    ${"/activate/123"} | ${"login-page"}
+    ${"/activate/123"} | ${"user-page"}
   `(
-    "does not display $pageTestId when path is $path", async ({ path, pageTestId }) => {
+    "does not display $pageTestId when path is $path",
+    async ({ path, pageTestId }) => {
       await setup(path);
       const page = screen.queryByTestId(pageTestId);
       expect(page).not.toBeInTheDocument();
@@ -55,26 +66,28 @@ describe("Routing", () => {
     await setup("/");
     const link = screen.queryByRole("link", { name: targetPage });
     expect(link).toBeInTheDocument();
-  })
+  });
 
   it.each`
     initialPath  | clickingTo   | visiblePage
     ${"/"}       | ${"Sign Up"} | ${"signup-page"}
     ${"/signup"} | ${"Home"}    | ${"home-page"}
-    ${"/"}       | ${"Login"}    | ${"login-page"}
-  `("displays sign up page after clicking sign up link", 
+    ${"/"}       | ${"Login"}   | ${"login-page"}
+  `(
+    "displays sign up page after clicking sign up link",
     async ({ initialPath, clickingTo, visiblePage }) => {
-    await setup(initialPath);
-    const link = screen.queryByRole("link", { name: clickingTo });
-    await userEvent.click(link);
-    const page = await screen.findByTestId(visiblePage);
-    expect(page).toBeInTheDocument();
-  });
+      await setup(initialPath);
+      const link = screen.queryByRole("link", { name: clickingTo });
+      await userEvent.click(link);
+      const page = await screen.findByTestId(visiblePage);
+      expect(page).toBeInTheDocument();
+    }
+  );
   it("displays home page when clicking brand logo", async () => {
     await setup("/login");
     const image = screen.queryByAltText("Hoaxify Logo");
     await userEvent.click(image);
     const page = await screen.findByTestId("home-page");
     expect(page).toBeInTheDocument();
-  })
+  });
 });
