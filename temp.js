ba7dc14f277b22bb873e8fddae6dd5e3a1325075
Santino <santino.cervera@gmail.com>
2022-06-23 17:01:02 +0300

    33.1rd Refactor Page with temp.js

--- temp.js
+++ temp.js
@@ -1,22 +1,3 @@
-import UserList from "./UserList.vue";
-import { render, screen } from "@testing-library/vue";
-import { setupServer } from "msw/node";
-import { rest } from "msw";
-
-const server = setupServer(
-  rest.get("/api/1.0/users", (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(getPage(0, 3)));
-  })
-);
-
-beforeAll(() => server.listen());
-
-beforeEach(() => {
-  server.resetHandlers();
-});
-
-afterAll(() => server.close());
-
 const getPage = (page, size) => {
   let start = page * size;
   let end = start + size;
@@ -39,11 +20,4 @@ const users = [
   { id: 7, username: "user7", email: "user7@mail.com", image: null },
 ];
 
-describe("User List", () => {
-
-  it("displays three users in list", async () => {
-    render(UserList);
-    const users = await screen.findAllByText(/user/);
-    expect(users.length).toBe(3);
-  })
-})
+console.log(getPage(0, 3));
