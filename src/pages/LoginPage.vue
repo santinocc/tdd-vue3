e7188299309cf325e69da24e5d3d1348c1073602
Santino <santino.cervera@gmail.com>
2022-06-25 15:25:55 +0300

    51th Vuex

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -63,8 +63,9 @@ export default {
     async submit() {
       this.apiProgress = true;
       try {
-        await login({ email: this.email, password: this.password });
+        const response = await login({ email: this.email, password: this.password });
         this.$router.push("/");
+        this.$store.commit('loginSuccess', response.data.id);
       } catch (error) {
         this.failMessage = error.response.data.message;
       }
